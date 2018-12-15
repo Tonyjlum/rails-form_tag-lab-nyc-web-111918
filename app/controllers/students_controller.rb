e956523e9bfb4e0c3def71bc5b278673f16ebeec
Tonyjlum <tonyjlum@gmail.com>
2018-12-15 14:55:55 -0500

    forms with from_tag

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+
+  end
+
+  # def new
+  #   @student = Student.new
+  #   render :new
+  # end
+
+  def create
+    #use individual params when using form_tag
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to "/students"
   end
 
   def show
